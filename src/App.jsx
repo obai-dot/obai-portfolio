b6b60c40b2729922af3188e703ff531eae375a0b
obai <obaieyad@gmail.com>
2026-09-14 08:44:13 -0700

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Skills from "./components/skills/Skills";
 import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       </main>
 
       <Footer />
+      <Analytics />
     </>
   );
 }
